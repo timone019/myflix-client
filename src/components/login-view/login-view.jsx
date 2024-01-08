746b26922a6628d481cb60d670906962ae648c3f
AJ Sorbello <ajsorbello@gmail.com>
2024-01-08 13:31:19 -0800

    created login view and signup

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password,
+    };
+
+    fetch("https://openlibrary.org/account/login.json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+
+        if (data.user) {
+          // Store user and token in local storage
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+
+          // Call onLoggedIn with user and token
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch(() => {
+        alert("Something went wrong");
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          minLength="6"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          minLength="8"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
