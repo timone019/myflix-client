43e59a7cc97bb11dcfdc2dc544f260ff00babb79
AJ Sorbello <ajsorbello@gmail.com>
2024-01-27 09:57:30 -0800

    updated likes to show on main movie profile

---
+++ src/components/navigation-bar/navigation-bar 2.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>Movie App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {!user && (
+              <>
+                <Nav.Link as={Link} to="/">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signup">
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+            {user && (
+              <>
+                <Nav.Link as={Link} to="/">
+                  All Movies
+                </Nav.Link>
+                <Nav.Link as={Link} to="/profile">
+                  Profile
+                </Nav.Link>
+                <Nav.Link onClick={() => onLoggedOut()} as={Link} to="/">
+                  Logout
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
---
+++ src/components/profile-view/profile-view 2.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
+import { MovieCard } from "../movie-card/movie-card";
+import UserInfo from "./user-info";
+import UpdateUser from "./update-user";
+
+export function ProfileView({
+  movies,
+  user,
+  setUser,
+  addFav,
+  removeFav,
+  favMovies,
+}) {
+  console.log(user);
+
+  const [favoriteMovies, setFavoriteMovies] = useState(favMovies);
+
+  useEffect(() => {
+    setFavoriteMovies(favMovies);
+  }, [favMovies]);
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} sm={4}>
+          <Card>
+            <Card.Body>
+              {user && (
+                <UserInfo
+                  username={user.Username}
+                  email={user.Email}
+                  birthday={user.Birthday}
+                />
+              )}
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={8}>
+          <Card>
+            <Card.Body>
+              <UpdateUser user={user} setUser={setUser} />
+            </Card.Body>
+          </Card>
+        </Col>
+        <></>
+        <h4>Favorite Movies</h4>
+        {movies
+          .filter((movie) => favoriteMovies.includes(movie._id))
+          .map((movie) => (
+            <Col className="mb-4" key={movie._id} md={3}>
+              <MovieCard
+                movie={movie}
+                addFav={addFav}
+                removeFav={removeFav}
+                isFav={favoriteMovies.includes(movie._id)}
+              />
+            </Col>
+          ))}
+      </Row>
+    </Container>
+  );
+}
